0229368bed719bd67b63b947f6d08d0681b5567c
Luca Citta <lucacitta@gmail.com>
2024-09-07 17:17:47 +0200

    [ UPDATE ] Staus bar upgrade

--- ui/elements/status_bar.py
+++ ui/elements/status_bar.py
@@ -1,7 +1,7 @@
 import pygame
 
 from elements.fonts import big_font, small_medium_font
-from elements.colors import white
+from elements.colors import black
 from utils import draw_text
 
 
@@ -11,40 +11,37 @@ def draw_status_bar(session_data):
     screen_width = screen.get_width()
     screen_height = screen.get_height()
     status_bar_x = 0
-    status_bar_y = screen_height - 180
+    status_bar_y = screen_height - 200
 
-    pygame.draw.rect(
-        screen, (0, 0, 0),
-        (
-            status_bar_x,
-            status_bar_y,
-            screen_width,
-            180
-        )
+    status_bar_image = pygame.transform.scale(
+        pygame.image.load('ui/assets/backgrounds/status_bar.png'),
+        (screen_width, 200)
     )
+    screen.blit(status_bar_image, (status_bar_x, status_bar_y))
+
 
     hero_image = pygame.transform.scale(
         pygame.image.load(session_data['player_image']),
         (100, 165)
     )
-    screen.blit(hero_image, (status_bar_x + 10, status_bar_y + 10))
+    screen.blit(hero_image, (status_bar_x + 20, status_bar_y + 20))
 
     draw_text(
         text=f'{session_data["player_name"]}',
         font=big_font,
-        text_color=(255, 255, 255),
+        text_color=black,
         surface=screen,
         x=status_bar_x + 200,
-        y=status_bar_y + 20,
+        y=status_bar_y + 25,
     )
 
     draw_text(
         text=f'HP: {hero.vida}/{hero.vidaMaxima}\nDaño: {hero.danio}\nArmadura: {hero.armadura}\nAgilidad: {hero.agilidad}\nPociones: {hero.pociones}/{hero.pocionesMaximas}\nMonedas: {hero.oro}',
         font=small_medium_font,
-        text_color=(255, 255, 255),
+        text_color=black,
         surface=screen,
         x=status_bar_x + 200,
-        y=status_bar_y + 50,
+        y=status_bar_y + 55,
     )
 
     equipment = [
@@ -68,35 +65,35 @@ def draw_status_bar(session_data):
     for item in equipment:
         item_attributes = hero.get_equipment(item['equipment_type'])
 
+        draw_text(
+            text=f'{item["equipment_type"]}',
+            font=big_font,
+            text_color=black,
+            surface=screen,
+            x=item['x'],
+            y=status_bar_y + 25,
+        )
+
         item_image = pygame.transform.scale(
             pygame.image.load(f'ui/assets/equipment/{item["image"]}'),
             (40, 40)
         )
 
-        screen.blit(item_image, (item['x'] - 20, status_bar_y + 20 + 10))
+        screen.blit(item_image, (item['x'] - 20, status_bar_y + 40))
 
         for key, value in item_attributes.items():
             if key == 'Nombre':
-                text = f'{value}'
+                text = f'Testing name'
             else:
                 text += f'\n{key}: {value}'
 
         draw_text(
-            text=f'{item["equipment_type"]}',
-            font=big_font,
-            text_color=white,
-            surface=screen,
-            x=item['x'],
-            y=status_bar_y + 20,
-        )
-
-        draw_text(
             text=text,
             font=small_medium_font,
-            text_color=white,
+            text_color=black,
             surface=screen,
             x=item['x'],
-            y=status_bar_y + 80,
+            y=status_bar_y + 90,
         )
 
     return session_data
