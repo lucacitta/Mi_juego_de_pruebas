1099329e593f1d9243cda97f27b4052ad4b2c92f
Luca Citta <lucacitta@gmail.com>
2024-09-21 20:07:11 +0200

    [ UPDATE ] Name selection UI

--- ui/states/name_selection.py
+++ ui/states/name_selection.py
@@ -1,40 +1,39 @@
 import pygame
 
 from elements.fonts import big_font, medium_font
-from elements.colors import black, white, gray
+from elements.colors import black
 from utils import draw_text
 
-def name_selection(session_data):
-    screen = session_data['screen']
-    screen_width = screen.get_width()
-    screen_height = screen.get_height()
-    text_input_active = session_data['text_input_active']
-    player_name = session_data['player_name']
-    input_box = session_data['buttons']['input_box']
-    continue_button = session_data['buttons']['continue_button']
+def draw_name_ask(screen, screen_width):
+    question_box_rect_x = screen_width // 2 - 200
+    question_box_rect_y = 200
+    question_box_image = pygame.transform.scale(
+        pygame.image.load('ui/assets/backgrounds/horizontal_message.png'),
+        (400, 200)
+    )
+    screen.blit(question_box_image, (question_box_rect_x, question_box_rect_y))
 
     draw_text(
-        text='Cual es tu nombre, viajero?',
-        font=big_font,
-        text_color=black,
-        surface=screen,
-        x=screen_width // 2, 
-        y=160,
-        bg_color=white
+        'Cual es tu nombre, viajero?',
+        big_font,
+        black,
+        screen,
+        question_box_rect_x + 200,
+        question_box_rect_y + 50,
     )
 
-    input_box.move_to(
-        x=screen_width // 2 - 140,
-        y=screen_height // 2
+def draw_name_input_box(screen, screen_width, screen_height, player_name, input_box):
+    input_box_bg_image = pygame.transform.scale(
+        pygame.image.load('ui/assets/backgrounds/horizontal_message.png'),
+        (300, 40)
     )
+
+    input_box_x = screen_width // 2 - 150
+    input_box_y = screen_height // 2
+    screen.blit(input_box_bg_image, (input_box_x, input_box_y))
+
+    input_box.move_to(input_box_x, input_box_y)
     input_box.action = lambda session_data: session_data.update({'text_input_active': True})
-
-    input_box_rect_color = gray if text_input_active else white
-    input_box.rect = pygame.draw.rect(
-        surface=screen,
-        color=input_box_rect_color,
-        rect = input_box.rect,
-    )
 
     text_surface = medium_font.render(player_name, True, black)
     text_width = text_surface.get_width()
@@ -45,13 +44,31 @@ def name_selection(session_data):
 
     screen.blit(text_surface, (text_x, text_y))
 
+def draw_continue_button(screen, screen_width, is_player_name_valid, input_box, session_data):
+    continue_button = session_data['buttons']['continue_button']
     continue_button.move_to(screen_width * 0.8 - continue_button.width // 2, 500)
     screen.blit(
-        continue_button.image if player_name else continue_button.disable_image,
+        continue_button.image if is_player_name_valid else continue_button.disable_image,
         (continue_button.x, continue_button.y)
     )
     continue_button.action = lambda session_data: session_data.update({'actual_state': 'class_selection'})
 
-    session_data['active_buttons'] = [input_box, continue_button] if player_name else [input_box]
+    session_data['active_buttons'] = [input_box, continue_button] if is_player_name_valid else [input_box]
+    return session_data
+
+def name_selection(session_data):
+    screen = session_data['screen']
+    screen_width = screen.get_width()
+    screen_height = screen.get_height()
+    player_name = session_data['player_name']
+    input_box = session_data['buttons']['input_box']
+
+    is_player_name_valid = player_name and len(player_name) >= 3
+
+    draw_name_ask(screen, screen_width)
+
+    draw_name_input_box(screen, screen_width, screen_height, player_name, input_box)
+
+    draw_continue_button(screen, screen_width, is_player_name_valid, input_box, session_data)
 
     return session_data
